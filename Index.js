41fe16654604386cc1a26db0f3d2614fe200c8ae
Proliferato <claudiovpcraveiro@hotmail.com>
2020-04-10 14:32:10 -0300

    Aula 2 finalizada

--- Index.js
+++ Index.js
@@ -1,6 +1,26 @@
 // AULA 1 - Criar constantes e mostrar na tela do console
 
-const nome = 'Claudio'
-var idade =  30
+// const nome = 'Claudio'
+// var idade =  30
 
-console.log(nome + ", sua idade é " + idade);
+// console.log(nome + ", sua idade é " + idade);
+
+// AULA 2 - Criar um programa que calcula a média das notas dos alunos 
+// e envia mensagem do cálculo da média.
+
+//const nome = "Claudio" //constante
+//const nome1 = 'Vinicius' //constante
+//const nome2 = `Claudio ${nome1}` //template string
+
+//console.log (nome2)
+
+//const aluno1 = 'Claudio'
+//const aluno2 = 'Victor'
+//const aluno3 = 'Fulano'
+//const nota_aluno1 = 9.5
+//const nota_aluno2 = 10
+//const nota_aluno3 = 3
+
+//const media = (nota_aluno1 + nota_aluno2 + nota_aluno3)/3
+
+//console.log ('Média dos alunos é ' + media)
